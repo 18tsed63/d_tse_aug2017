4893d5996e637064594478fa60847ad887ed68d2
18tsed63 <18tsed63@millshighsch.org>
2017-08-23 11:22:18 -0700

    hi

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -2,13 +2,14 @@
 /*
 *first hello world class
 *@author Darius Tse
-*@version Neon 4.6 8/21/2017
+*@version Oxygen 4.7 8/23/2017
 */
 public class Hello_World {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println ("Hello World! From <Darius Tse>");
+	//Display the string
 	}
 
 }
