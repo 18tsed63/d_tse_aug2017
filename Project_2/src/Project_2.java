5bf8004a0bfa21b724803e178571823b64cba8f1
18tsed63 <18tsed63@millshighsch.org>
2017-08-24 09:30:35 -0700

    ?????

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,15 @@
+// Darius Tse
+/* 8/24/2017 */
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+System.out.println("Hello");
+	
+	
+	
+	
+	
+	}
+
+}
