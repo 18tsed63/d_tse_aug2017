b0b0978b84581657f5536bd84366f8264c05ac64
18tsed63 <18tsed63@millshighsch.org>
2017-08-30 11:30:31 -0700

    akdk;fj

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,16 @@
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Pikachu welcomes you to the world of Pokemon");
+System.out.println("                (\\__/)");
+System.out.println("               (o^ . ^)");
+System.out.println("             z(_(\")(\")_)");
+	
+	
+	
+	
+	}
+
+}
