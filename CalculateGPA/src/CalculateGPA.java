dd2b2132d41252dd29745e8502a6ab008bccc65c
18tsed63 <18tsed63@millshighsch.org>
2017-09-03 22:28:35 -0700

    probs not right

---
+++ CalculateGPA/src/CalculateGPA.java
@@ -0,0 +1,33 @@
+
+public class CalculateGPA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// declare variables
+		double grade1;
+		double grade2;
+		double grade3;
+		double gpa;
+		
+		// compute GPA
+		grade1 = 2.0;
+		grade2 = 3.0;
+		grade3 = 4.0;
+		gpa = (grade1 + grade2 + grade3)/3;
+		
+		// print results
+		System.out.println("Your GPA:");
+		System.out.println(gpa);
+		
+		//lol :) :) :) trying something out
+		grade1 = 3.0;
+		grade2 = 3.5;
+		gpa = (grade1 + grade2 + grade3)/3;
+		
+		System.out.println("GPA if u were smarter");
+		System.out.println(gpa);		
+		
+	}
+
+}
