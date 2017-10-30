9927e16597613d3d7be0a6f698b69fca45e31edc
18tsed63 <31107058+18tsed63@users.noreply.github.com>
2017-10-30 16:56:12 -0700

    Update Split.java

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -25,15 +25,20 @@ public class Split{
 		System.out.println(Arrays.toString("I like apples".split(" ")));
 		System.out.println(Arrays.toString("I really like really red apples".split("really")));
 		System.out.println(Arrays.toString("really I really like really red really apples, really".split("really")));
+		System.out.println(Arrays.toString("apples pineapples bread lettus tomato bacon mayo ham bread cheese" .split("bread")));
+		//get the filling
 		System.out.println(getTheFilling("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
 		System.out.println(getTheFilling("applespineapplesbreadlettustomatobreadbaconbreadmayohambreadcheese"));
+		System.out.println(getTheFilling("apples pineapples bread lettus tomato bread bacon mayo ham bread cheese" ));
+		
 		//Your task Part 1:
 		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
 		* use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		* What if it's a fancy sandwich with multiple pieces of bread?
 		*/
 	}
-		public static String getTheFilling(String sandwich) {
+	//PART 1 and 2 (this code works for both)	
+	public static String getTheFilling(String sandwich) {
 			String [] i = sandwich.split("bread");
 			String filling = "";
 			//not a sandwich
@@ -54,6 +59,6 @@ public class Split{
 			
 			
 			return filling;	
-	}
+	}		
 	
 }
