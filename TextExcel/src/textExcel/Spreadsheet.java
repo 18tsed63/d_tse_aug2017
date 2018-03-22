91aede32127050eb268523f0dd97e386752c5fa7
18tsed63 <18tsed63@millshighsch.org>
2018-03-22 09:51:57 -0700

    checkpoint 2 done

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -9,47 +9,46 @@ import java.util.Arrays;
 
 // Update this file with your own code.
 
-public class Spreadsheet implements Grid
-{
+public class Spreadsheet implements Grid {
 	private Cell[][] sheet = new Cell[20][12];
-	
+
 	public Spreadsheet() {
-		for(int i = 0; i < sheet.length; i++) {
-			for(int j = 0; j < sheet[i].length; j++) {
+		for (int i = 0; i < sheet.length; i++) {
+			for (int j = 0; j < sheet[i].length; j++) {
 				Cell k = new EmptyCell();
 				sheet[i][j] = k;
 			}
 		}
 	}
+
 	@Override
-	public String processCommand(String command)
-	{
-		//command = command.toLowerCase();
+	public String processCommand(String command) {
+		// command = command.toLowerCase();
 		String result = "";
-		String [] arr = command.split(" ");
-		//makes it so commands are not sensitive to upper or lower case
+		String[] arr = command.split(" ");
+		// makes it so commands are not sensitive to upper or lower case
 		arr[0] = arr[0].toLowerCase();
 		SpreadsheetLocation cell = new SpreadsheetLocation(arr[0]);
 		int row = 0;
 		int col = 0;
-		//if(command.indexOf("clear") < 0) {
-		if(!arr[0].equals("clear")) {
-		row = cell.getRow();
-		col = cell.getCol();
+		// if(command.indexOf("clear") < 0) {
+		if (!arr[0].equals("clear")) {
+			row = cell.getRow();
+			col = cell.getCol();
 		}
-		//clear command
-		if(arr[0].equals("clear")) {
-		//if(command.indexOf("clear") >= 0) {
-			//clear cell
-			
-			if(arr.length > 1) {
+		// clear command
+		if (arr[0].equals("clear")) {
+			// if(command.indexOf("clear") >= 0) {
+			// clear cell
+
+			if (arr.length > 1) {
 				SpreadsheetLocation clearCell = new SpreadsheetLocation(arr[1]);
 				sheet[clearCell.getRow()][clearCell.getCol()] = new EmptyCell();
 			}
-			//clear grid
-			else{
-				for(int i = 0; i < sheet.length; i++) {
-					for(int j = 0; j < sheet[i].length; j++) {
+			// clear grid
+			else {
+				for (int i = 0; i < sheet.length; i++) {
+					for (int j = 0; j < sheet[i].length; j++) {
 						Cell k = new EmptyCell();
 						sheet[i][j] = k;
 					}
@@ -57,51 +56,47 @@ public class Spreadsheet implements Grid
 			}
 			result = getGridText();
 		}
-		//cell inspection command
-		else if(arr.length == 1) {
+		// cell inspection command
+		else if (arr.length == 1) {
 			result = sheet[row][col].fullCellText();
 		}
-		//cell assignment command
-		else if(arr.length > 1){
-			String [] texts = command.split(" ", 3);
+		// cell assignment command
+		else if (arr.length > 1) {
+			String[] texts = command.split(" ", 3);
 			sheet[row][col] = new TextCell(texts[2]);
 			result = getGridText();
-			
+
 		}
 		return result;
 	}
 
 	@Override
-	public int getRows()
-	{
+	public int getRows() {
 		return 20;
 	}
 
 	@Override
-	public int getCols()
-	{
+	public int getCols() {
 		return 12;
 	}
 
 	@Override
-	public Cell getCell(Location loc)
-	{
+	public Cell getCell(Location loc) {
 		return sheet[loc.getRow()][loc.getCol()];
 	}
 
 	@Override
-	public String getGridText()
-	{
+	public String getGridText() {
 		String grid = "   ";
-		for(char k = 'A'; k <= 'L'; k++) {
+		for (char k = 'A'; k <= 'L'; k++) {
 			grid += "|" + k + "         ";
 		}
 		grid += "|\n";
-		
-		for(int i = 0; i < sheet.length; i++) {
+
+		for (int i = 0; i < sheet.length; i++) {
 			String number = i + 1 + "   ";
 			grid += number.substring(0, 3);
-			for(int j = 0; j < sheet[i].length; j++) {
+			for (int j = 0; j < sheet[i].length; j++) {
 				grid += "|" + sheet[i][j].abbreviatedCellText();
 			}
 			grid += "|\n";
@@ -110,4 +105,3 @@ public class Spreadsheet implements Grid
 	}
 
 }
-
